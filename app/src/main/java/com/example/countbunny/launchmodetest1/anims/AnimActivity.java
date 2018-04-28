215e3bba8b36b14ac092604b20018968b889a9bc
countbunny <happy_zoo@126.com>
2018-04-28 21:55:42 +0800

    animation chapter finished

--- app/src/main/java/com/example/countbunny/launchmodetest1/anims/AnimActivity.java
+++ app/src/main/java/com/example/countbunny/launchmodetest1/anims/AnimActivity.java
@@ -1,5 +1,8 @@
 package com.example.countbunny.launchmodetest1.anims;
 
+import android.animation.IntEvaluator;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.TypedValue;
@@ -44,5 +47,52 @@ public class AnimActivity extends AppCompatActivity {
         textView.setAnimation(a);
         mContainer.addView(textView);
         a.start();
+    }
+
+    public void changeWidth1(View view) {
+        performAnimate(view);
+    }
+
+    private void performAnimate(View view) {
+        ViewWrapper wrapper = new ViewWrapper(view);
+        ObjectAnimator.ofInt(wrapper, "width", 500).setDuration(5000).start();
+    }
+
+    public void changeWidth2(View view) {
+        performAnimate(view, 200, 600);
+    }
+
+    private void performAnimate(final View target, final int start, final int end) {
+        final ValueAnimator valueAnimator = ValueAnimator.ofInt(1, 100);
+        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            private IntEvaluator mEvaluator = new IntEvaluator();
+
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                int currentValue = (Integer) animation.getAnimatedValue();
+                float fraction = animation.getAnimatedFraction();
+                target.getLayoutParams().width = mEvaluator.evaluate(fraction, start, end);
+                target.requestLayout();
+            }
+        });
+        valueAnimator.setDuration(5000).start();
+    }
+
+    private static class ViewWrapper {
+
+        private View mTarget;
+
+        public ViewWrapper(View target) {
+            mTarget = target;
+        }
+
+        public int getWidth() {
+            return mTarget.getLayoutParams().width;
+        }
+
+        public void setWidth(int width) {
+            mTarget.getLayoutParams().width = width;
+            mTarget.requestLayout();
+        }
     }
 }
