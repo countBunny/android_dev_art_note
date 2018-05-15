e988615ae8b7a9b6ef95b0f6ffbb5b46bb04eadc
countbunny <happy_zoo@126.com>
2018-05-16 00:26:59 +0800

    Crash handler added

---
+++ app/src/main/java/com/example/countbunny/launchmodetest1/othertips/CrashHandler.java
@@ -0,0 +1,121 @@
+package com.example.countbunny.launchmodetest1.othertips;
+
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Environment;
+import android.os.Process;
+import android.util.Log;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class CrashHandler implements Thread.UncaughtExceptionHandler {
+
+    private static final String TAG = "CrashHandler";
+    private static final boolean DEBUG = true;
+
+    private static final String PATH = Environment.getExternalStorageDirectory().getPath() + "/CrashTest/log/";
+    private static final String FILE_NAME = "crash";
+    private static final String FILE_NAME_SUFFIX = ".trace";
+
+    private static CrashHandler sInstance = new CrashHandler();
+    private Thread.UncaughtExceptionHandler mDefaultCrashHandler;
+    private Context mContext;
+
+    private CrashHandler() {
+    }
+
+    private static CrashHandler getInstance() {
+        return sInstance;
+    }
+
+    public void init(Context context) {
+        mDefaultCrashHandler = Thread.getDefaultUncaughtExceptionHandler();
+        Thread.setDefaultUncaughtExceptionHandler(this);
+        mContext = context.getApplicationContext();
+    }
+
+    @Override
+    public void uncaughtException(Thread t, Throwable e) {
+        try {
+            dumpExceptionToSDCard(e);
+            uploadExceptionToServer();
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+
+        e.printStackTrace();
+        //如果程序提供了默认异常处理器， 交给系统去结束程序， 否则自己来结束自己
+        if (mDefaultCrashHandler != null) {
+            mDefaultCrashHandler.uncaughtException(t, e);
+        } else {
+            Process.killProcess(Process.myPid());
+        }
+    }
+
+    private void uploadExceptionToServer() {
+        //TODO Upload Exception Message To Your Web Server
+    }
+
+    private void dumpExceptionToSDCard(Throwable e) throws IOException {
+        if (!Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+            if (DEBUG) {
+                Log.w(TAG, "sdcard unmounted, skip dump exception");
+                return;
+            }
+        }
+
+        File dir = new File(PATH);
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+        long current = System.currentTimeMillis();
+        String time = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date(current));
+        File file = new File(PATH + FILE_NAME + time + FILE_NAME_SUFFIX);
+        PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(file)));
+        pw.println(time);
+        try {
+            dumpPhoneInfo(pw);
+        } catch (PackageManager.NameNotFoundException e1) {
+            Log.e(TAG, "dump phone info failed");
+        }
+        pw.println();
+        e.printStackTrace(pw);
+        pw.close();
+
+    }
+
+    private void dumpPhoneInfo(PrintWriter pw) throws PackageManager.NameNotFoundException {
+        PackageManager pm = mContext.getPackageManager();
+        PackageInfo pi = pm.getPackageInfo(mContext.getPackageName(), PackageManager.GET_ACTIVITIES);
+        pw.print("App version: ");
+        pw.print(pi.versionName);
+        pw.print('_');
+        pw.println(pi.versionCode);
+
+        //Android 版本号
+        pw.print("OS Version :");
+        pw.print(Build.VERSION.RELEASE);
+        pw.print('_');
+        pw.println(Build.VERSION.SDK_INT);
+
+        //手机制造商
+        pw.print("Vendor: ");
+        pw.println(Build.MANUFACTURER);
+
+        //手机型号
+        pw.print("Model: ");
+        pw.println(Build.MODEL);
+
+        //CPU架构
+        pw.print("CPU ABI: ");
+        pw.println(Build.SUPPORTED_ABIS);
+    }
+}
