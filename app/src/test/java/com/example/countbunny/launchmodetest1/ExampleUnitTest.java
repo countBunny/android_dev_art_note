c87e8f7aed27a022d484e996e84305a12f9c0418
countbunny <happy_zoo@126.com>
2019-02-12 00:10:31 +0800

    Test some java grammar

--- app/src/test/java/com/example/countbunny/launchmodetest1/ExampleUnitTest.java
+++ app/src/test/java/com/example/countbunny/launchmodetest1/ExampleUnitTest.java
@@ -4,6 +4,7 @@ package com.example.countbunny.launchmodetest1;
 import org.junit.Test;
 
 import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
 
 
 /**
@@ -12,8 +13,59 @@ import static junit.framework.Assert.assertEquals;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+    static int B = 1;
+
+    static {
+        B = 2;
+        System.out.print(B);
+    }
+
+    interface Interface0 {
+        int A = 0;
+    }
+
+    interface Interface1 extends Interface0 {
+        int A = 1;
+    }
+
+    interface Interface2 {
+        int A = 2;
+    }
+
+    static class Parent implements Interface1 {
+        public static final int A = 3;
+
+    }
+
+    static class Stub implements Interface2 {
+        public static final int A = 4;
+    }
+
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
+//        System.out.print(Stub.A);
+    }
+
+    @Test
+    public void whatEqualA() {
+        assertTrue(Stub.A == 4);
+    }
+
+    @Test
+    public void autoPackage() {
+        Integer a = 1;
+        Integer b = 2;
+        Integer c = 3;
+        Integer d = 3;
+        Integer e = 321;
+        Integer f = 321;
+        Long g = 3L;
+        System.out.println(" c == d :" + (c == d));
+        System.out.println(" e == f :" + (e == f));
+        System.out.println(" c == (a + b) :" + (c == (a + b)));
+        System.out.println(" c.equals(a + b) :" + (c.equals(a + b)));
+        System.out.println(" g == a + b :" + (g == a + b));
+        System.out.println(" g.equals(a + b) :" + (g.equals(a + b)));
     }
 }
